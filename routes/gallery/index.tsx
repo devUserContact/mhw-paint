a79c34ea05775983a3d3384b35d51ba1a38a405e
devUserContact <developer.user.contact@protonmail.com>
2024-05-15 08:45:22 -0700

    map vaules from `.toml` file

--- routes/gallery/index.tsx
+++ routes/gallery/index.tsx
@@ -1,15 +1,18 @@
-import { parse } from "https://deno.land/std@0.224.0/toml/mod.ts";
+import { parse, stringify } from "https://deno.land/std@0.224.0/toml/mod.ts";
 import NavBar from "../../components/NavBar/index.tsx";
 
 const Gallery = await Deno.readTextFile("./assets/Gallery.toml");
+const gallery = parse(Gallery);
 
 export default function GalleryPage() {
   return (
     <div>
       <NavBar />
-      <div className="grid">
+      <div classname="grid">
         <p>
-          {parse(Gallery)}
+          {gallery.work.map((work) => {
+            return <p>{work.title}</p>;
+          })}
         </p>
       </div>
     </div>
