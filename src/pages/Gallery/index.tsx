85f1b5ec4a6d1bc23d4b9c9f72e85b80a635ce0a
devUserContact <developer.user.contact@protonmail.com>
2024-05-16 11:30:52 -0700

    debugging `gallery.json`

--- src/pages/Gallery/index.tsx
+++ src/pages/Gallery/index.tsx
@@ -1,4 +1,9 @@
 import type { Component } from 'solid-js';
+import Works from '../../assets/gallery.json'
+
+let work= Works.work;
+
+console.log(work[0].title)
 
 export default function Gallery() {
     return(
