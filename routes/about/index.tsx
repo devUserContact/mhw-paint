146a0e15b3053378f405fc80f005910a4c89d1e9
devUserContact <developer.user.contact@protonmail.com>
2024-05-15 17:01:18 -0700

    added text to `about` page

--- routes/about/index.tsx
+++ routes/about/index.tsx
@@ -4,7 +4,14 @@ export default function AboutPage() {
   return (
     <div>
       <NavBar />
-      about
+      <div className="grid grid-cols-1 mt-10 place-items-center">
+        <h1 className="w-1/2 text-2xl">
+          I go by Maxwell-
+        </h1>
+        <p className="mt-5 w-1/2 text-m">
+          I am a human and a painter.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
