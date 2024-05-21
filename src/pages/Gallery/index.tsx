4129a7e23be0829d1fad190e2b02460081a8244f
devUserContact <developer.user.contact@protonmail.com>
2024-05-20 19:49:24 -0700

    implemented tailwind `work.title` on hover

--- src/pages/Gallery/index.tsx
+++ src/pages/Gallery/index.tsx
@@ -3,18 +3,20 @@ import { For } from "solid-js";
 import Works from "../../assets/gallery.json";
 
 let work = Works.work;
- 
+
 export default function Gallery() {
   return (
-    <div className="grid grid-cols-1 place-items-center md:grid-cols-2">
+    <div class="grid grid-cols-1 ml-10 mr-10 place-items-center md:grid-cols-2">
       <For each={work}>
         {(work, index) => (
-          <div className="grid w-3/4 border place-items-center">
-            <img className="h-40" src={work.image_url} tag={work.title} />
-            <p>{work.title}</p>
+          <div class="relative grid w-auto place-items-center group">
+            <img class="h-4/5 group-hover:blur-sm" src={work.image_url} alt={work.title} />
+            <div class="absolute w-3/5 h-4/5 opacity-0 group-hover:opacity-100">
+              <p class="mt-10 text-xl text-left">{work.title}</p>
+            </div>
           </div>
         )}
       </For>
-    </div> 
+    </div>
   );
 }
