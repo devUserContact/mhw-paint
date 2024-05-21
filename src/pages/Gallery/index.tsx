1c0daf1831ada285905a49a204ad19621c924b56
devUserContact <developer.user.contact@protonmail.com>
2024-05-20 20:46:56 -0700

    added `work` card to gallery

--- src/pages/Gallery/index.tsx
+++ src/pages/Gallery/index.tsx
@@ -11,8 +11,13 @@ export default function Gallery() {
         {(work, index) => (
           <div class="relative grid w-auto place-items-center group">
             <img class="h-4/5 group-hover:blur-sm" src={work.image_url} alt={work.title} />
-            <div class="absolute w-3/5 h-4/5 opacity-0 group-hover:opacity-100">
-              <p class="mt-10 text-xl text-left">{work.title}</p>
+            <div class="absolute p-5 w-3/5 bg-slate-200 bg-opacity-50 opacity-0 group-hover:opacity-100">
+              <p class="text-xl text-left">mhw</p>
+              <p class="text-xl text-left italic">{work.title}</p>
+              <br/>
+              <p class="text-xl text-left">{work.year_created}</p>
+              <p class="text-xl text-left">{work.medium}</p>
+              <p class="text-xl text-left">{work.size[0]} x {work.size[1]} inches</p>
             </div>
           </div>
         )}
