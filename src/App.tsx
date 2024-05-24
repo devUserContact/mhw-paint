2ebc5e31f4454b1d9b723b1ac4753c8e1708abd9
devUserContact <developer.user.contact@protonmail.com>
2024-05-24 12:30:57 -0700

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,42 @@
-import './index.css';
+import './index.css'
 
-import { lazy } from "solid-js";
-import { render } from "solid-js/web";
-import { Router, Route } from "@solidjs/router";
+import { lazy } from 'solid-js'
+import { render } from 'solid-js/web'
+import { Router, Route } from '@solidjs/router'
 
-const root = document.getElementById('root');
+const root = document.getElementById('root')
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
     'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
-  );
+  )
 }
 
+const Home = lazy(() => import('./pages/Home'))
+const Gallery = lazy(() => import('./pages/Gallery'))
+const Work = lazy(() => import('./pages/Work'))
+const About = lazy(() => import('./pages/About'))
 
-const Home = lazy(() => import("./pages/Home"));
-const Gallery = lazy(() => import("./pages/Gallery"));
-const Work = lazy(() => import("./pages/Work"));
-const About = lazy(() => import("./pages/About"));
-
-const App = props => (
+const App = (props) => (
   <>
-    <nav class="grid grid-cols-3 pt-5 pb-5 bg-sky-300">
-      <a href="/">🎨</a>
-      <a href="/gallery">gallery</a>
-      <a href="/about">about</a>
+    <nav class='grid grid-cols-3 pt-5 pb-5 bg-sky-300'>
+      <a href='/'>🎨</a>
+      <a href='/gallery'>gallery</a>
+      <a href='/about'>about</a>
     </nav>
     {props.children}
   </>
-);
+)
 
 render(
   () => (
     <Router root={App}>
-      <Route path="/" component={Home} />
-      <Route path="/gallery" component={Gallery} />
-      <Route path="/gallery/:work" component={Work} />
-      <Route path="/about" component={About} />
+      <Route path='/' component={Home} />
+      <Route path='/gallery' component={Gallery} />
+      <Route path='/gallery/:work' component={Work} />
+      <Route path='/about' component={About} />
     </Router>
   ),
-  root!
-);
-export default App;
+  root!,
+)
+export default App
