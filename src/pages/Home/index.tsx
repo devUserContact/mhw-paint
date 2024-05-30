de5ffd07c0cd099c82e1938473a6587b9ef9924e
devUserContact <developer.user.contact@protonmail.com>
2024-05-30 08:13:37 -0700

    added meta tags

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,7 +5,8 @@ export default function Home() {
     <div>
       <div class='grid h-20 mt-20 place-items-center'>
         <h1 class='grid text-xl'>mhwPaint</h1>
-        <h1 class='grid text-center'>artist from CA</h1>
+        <h1 class='grid text-xl'>-</h1>
+        <h1 class='grid text-center'>an Artist from CA</h1>
       </div>
     </div>
   )
